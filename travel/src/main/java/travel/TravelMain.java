f4d5965f13ead608ae05bca7f2c31569cab56d74
Marcin Dzierzak <m@d.pl>
2025-02-10 10:21:46 +0100

    spring - xml app context

--- travel/src/main/java/travel/TravelMain.java
+++ travel/src/main/java/travel/TravelMain.java
@@ -1,9 +1,12 @@
 package travel;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import travel.impl.Bus;
 import travel.impl.Hotel;
 
 import java.time.LocalDate;
+
+import org.springframework.context.ApplicationContext;
 
 public class TravelMain {
 
@@ -12,14 +15,17 @@ public class TravelMain {
         Person doe = new Person("Joe", "Doe", new Ticket(LocalDate.now()));
 
         // service preparation
-        Transportation transportation = new Bus();
+  /*      Transportation transportation = new Bus();
         Accomodation accomodation = new Hotel();
         String travelName = "Holiday 2023";
 
         Travel travel = new Travel();
         travel.setName(travelName);
         travel.setTransportation(transportation);
-        travel.setAccomodation(accomodation);
+        travel.setAccomodation(accomodation);*/
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Travel travel = (Travel)context.getBean("travel");
 
         // service use
         travel.travel(doe);
