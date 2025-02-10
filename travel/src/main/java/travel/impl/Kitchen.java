b3ab366896b773d87b37e2c6ae10407a999ab41e
Marcin Dzierzak <m@d.pl>
2025-02-10 11:13:30 +0100

    spring - string bean & list bean

---
+++ travel/src/main/java/travel/impl/Kitchen.java
@@ -0,0 +1,18 @@
+package travel.impl;
+
+import org.springframework.beans.factory.FactoryBean;
+
+import java.util.List;
+
+public class Kitchen implements FactoryBean<List<String>> {
+
+    @Override
+    public List<String> getObject() throws Exception {
+        return List.of("pomidorowa", "schabowy", "sernik");
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return List.class;
+    }
+}
