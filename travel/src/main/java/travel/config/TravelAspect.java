4998ce43848d6ff19dd614f8fb41ffec48a853a2
Marcin Dzierzak <m@d.pl>
2025-02-11 11:19:45 +0100

    travel - aop - @Pointcut

--- travel/src/main/java/travel/config/TravelAspect.java
+++ travel/src/main/java/travel/config/TravelAspect.java
@@ -1,8 +1,10 @@
 package travel.config;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -11,10 +13,19 @@ import java.util.Arrays;
 @Aspect
 public class TravelAspect {
 
-    @Before("execution(public * travel.impl..*(..))")
+    @Pointcut("execution(public * travel.impl..*(..))")
+    void allPublicInImpl(){}
+
+    @Before("allPublicInImpl()")
     void logEnteringMethod(JoinPoint joinPoint) {
         System.out.println("[TRACE] Entering Method: " + joinPoint.getSignature().toString()
         + " on " + joinPoint.getTarget().getClass().getName() + " with " + Arrays.toString(joinPoint.getArgs()));
+    }
+
+    @After("allPublicInImpl()")
+    void logExitingMethod(JoinPoint joinPoint) {
+        System.out.println("[TRACE] Exiting Method: " + joinPoint.getSignature().toString()
+                + " on " + joinPoint.getTarget().getClass().getName() + " with " + Arrays.toString(joinPoint.getArgs()));
     }
 
 }
