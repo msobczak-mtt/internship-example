b638449233d7856ddcec5654d962e3f6020459d0
Marcin Dzierzak <m@d.pl>
2025-02-12 13:34:32 +0100

    vod - web - i18n - message source

--- app-vod/src/main/java/vod/web/VodAdvice.java
+++ app-vod/src/main/java/vod/web/VodAdvice.java
@@ -2,26 +2,33 @@ package vod.web;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @Slf4j
 @RequiredArgsConstructor
 public class VodAdvice {
 
+    private final MessageSource messageSource;
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    List<ObjectError> handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
+    List<String> handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
         log.error("argument not valid exception", exception);
 
-        return exception.getAllErrors();
+        Locale locale = Locale.getDefault();
+
+        return exception.getAllErrors().stream()
+                .map(oe->messageSource.getMessage(oe.getCode(), oe.getArguments(), locale))
+                .collect(Collectors.toList());
     }
 }
