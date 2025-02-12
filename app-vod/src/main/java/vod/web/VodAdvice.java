e15d1405c25024e1f1016994d7c0ff3b22687780
Marcin Dzierzak <m@d.pl>
2025-02-12 14:01:28 +0100

    vod - web - i18n - locale resolver

--- app-vod/src/main/java/vod/web/VodAdvice.java
+++ app-vod/src/main/java/vod/web/VodAdvice.java
@@ -1,5 +1,6 @@
 package vod.web;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.LocaleResolver;
 
 import java.util.List;
 import java.util.Locale;
@@ -19,13 +21,22 @@ import java.util.stream.Collectors;
 public class VodAdvice {
 
     private final MessageSource messageSource;
+    private final LocaleResolver localeResolver;
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    List<String> handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
+    List<String> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, HttpServletRequest request) {
         log.error("argument not valid exception", exception);
 
-        Locale locale = Locale.getDefault();
+        Locale locale = localeResolver.resolveLocale(request);
+                //new Locale("it", "IT");
+                //Locale.getDefault();
+
+        // messages_en_GB
+        // messages_en
+        // messages_<OS_LANG>_<OS_COUNTRY>
+        // messages_<OS_LANG>
+        // messages
 
         return exception.getAllErrors().stream()
                 .map(oe->messageSource.getMessage(oe.getCode(), oe.getArguments(), locale))
