f3ce4802d560b84d9ac7078c74c1b7b1dd51d82b
Marcin Dzierzak <m@d.pl>
2025-02-13 11:47:04 +0100

    vod - db - jdbc - movie & director daos

--- app-vod/src/main/java/vod/repository/jdbc/JdbcDirectorDao.java
+++ app-vod/src/main/java/vod/repository/jdbc/JdbcDirectorDao.java
@@ -1,25 +1,51 @@
 package vod.repository.jdbc;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 import vod.model.Director;
 import vod.repository.DirectorDao;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
+@Primary
+@RequiredArgsConstructor
 public class JdbcDirectorDao implements DirectorDao {
+
+    private static final String SELECT_ALL_DIRECTORS = "select d.id as director_id, d.firstname as director_first_name, d.lastname as director_last_name from director d";
+
+    public static final String SELECT_DIRECTOR_BY_ID = "select d.id as director_id, d.firstname as director_first_name, d.lastname as director_last_name from director d where id=?";
+
+    private final JdbcTemplate jdbcTemplate;
+
 
     @Override
     public List<Director> findAll() {
-        return List.of();
+        return jdbcTemplate.query(SELECT_ALL_DIRECTORS, new DirectorMapper());
     }
 
     @Override
     public Optional<Director> findById(Integer id) {
-        return Optional.empty();
+        return Optional.ofNullable(jdbcTemplate.queryForObject(SELECT_DIRECTOR_BY_ID, new DirectorMapper(), id));
     }
 
     @Override
     public Director save(Director d) {
         return null;
+    }
+
+    static class DirectorMapper implements RowMapper<Director> {
+        @Override
+        public Director mapRow(java.sql.ResultSet rs, int rowNum) throws java.sql.SQLException {
+            Director d = new Director();
+            d.setId(rs.getInt("director_id"));
+            d.setFirstName(rs.getString("director_first_name"));
+            d.setLastName(rs.getString("director_last_name"));
+            return d;
+        }
     }
 }
--- app-vod/src/main/java/vod/repository/jdbc/JdbcMovieDao.java
+++ app-vod/src/main/java/vod/repository/jdbc/JdbcMovieDao.java
@@ -1,6 +1,10 @@
 package vod.repository.jdbc;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 import vod.model.Cinema;
 import vod.model.Director;
 import vod.model.Movie;
@@ -13,40 +17,46 @@ import java.util.List;
 import java.util.Optional;
 
 
+@Repository
+@Primary
 @RequiredArgsConstructor
 public class JdbcMovieDao implements MovieDao {
 
-    private static final String SELECT_ALL_MOVIES =  "select m.id as movie_id, " +
+    private static final String SELECT_ALL_MOVIES = "select m.id as movie_id, " +
             "m.title as movie_title, m.poster as movie_poster, m.director_id as movie_director_id from movie m";
 
     public static final String SELECT_MOVIE_BY_ID = "select m.id as movie_id, " +
             "m.title as movie_title, m.poster as movie_poster, m.director_id as movie_director_id from movie m where id=?";
 
-    private static final String SELECT_MOVIES_BY_CINEMA =  "select m.id as movie_id, " +
+    private static final String SELECT_MOVIES_BY_CINEMA = "select m.id as movie_id, " +
             "m.title as movie_title, m.poster as movie_poster, m.director_id as movie_director_id " +
             "from movie m inner join movie_cinema mc on mc.movie_id=m.id " +
             "where mc.cinema_id=?";
 
-    private final DataSource dataSource;
+    private static final String SELECT_MOVIES_BY_DIRECTOR = "select m.id as movie_id, " +
+            "m.title as movie_title, m.poster as movie_poster, m.director_id as movie_director_id " +
+            "from movie m where m.director_id=?";
+
+    private final JdbcTemplate jdbcTemplate;
 
     @Override
     public List<Movie> findAll() {
-        return null;
+        return jdbcTemplate.query(SELECT_ALL_MOVIES, new MovieMapper());
     }
 
     @Override
     public Optional<Movie> findById(Integer id) {
-        return Optional.empty();
+        return Optional.ofNullable(jdbcTemplate.queryForObject(SELECT_MOVIE_BY_ID, new MovieMapper(), id));
     }
 
     @Override
     public List<Movie> findByDirector(Director d) {
-        return null;
+        return jdbcTemplate.query(SELECT_MOVIES_BY_DIRECTOR, new MovieMapper(), d.getId());
     }
 
     @Override
     public List<Movie> findByCinema(Cinema c) {
-        return null;
+        return jdbcTemplate.query(SELECT_MOVIES_BY_CINEMA, new MovieMapper(), c.getId());
     }
 
     @Override
@@ -54,14 +64,19 @@ public class JdbcMovieDao implements MovieDao {
         return null;
     }
 
-    public Movie mapMovie(ResultSet rs) throws SQLException {
-        Movie m = new Movie();
-        m.setId(rs.getInt("movie_id"));
-        m.setTitle(rs.getString("movie_title"));
-        m.setPoster(rs.getString("movie_poster"));
-        Director director = new Director();
-        director.setId(rs.getInt("movie_director_id"));
-        m.setDirector(director);
-        return m;
+
+    static class MovieMapper implements RowMapper<Movie> {
+
+        @Override
+        public Movie mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Movie m = new Movie();
+            m.setId(rs.getInt("movie_id"));
+            m.setTitle(rs.getString("movie_title"));
+            m.setPoster(rs.getString("movie_poster"));
+            Director director = new Director();
+            director.setId(rs.getInt("movie_director_id"));
+            m.setDirector(director);
+            return m;
+        }
     }
 }
