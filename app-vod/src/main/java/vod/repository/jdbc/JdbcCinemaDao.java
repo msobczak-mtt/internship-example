6e0db73e3505acfdc139421cdbb2de778c6d2be4
Marcin Dzierzak <m@d.pl>
2025-02-13 11:19:37 +0100

    vod - db - jdbc - cinema dao

--- app-vod/src/main/java/vod/repository/jdbc/JdbcCinemaDao.java
+++ app-vod/src/main/java/vod/repository/jdbc/JdbcCinemaDao.java
@@ -1,40 +1,43 @@
 package vod.repository.jdbc;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 import vod.model.Cinema;
 import vod.model.Movie;
 import vod.repository.CinemaDao;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-
+@Repository
+@Primary
 @RequiredArgsConstructor
 public class JdbcCinemaDao implements CinemaDao {
 
-    private static final String SELECT_ALL_CINEMAS =  "select c.id as cinema_id, " +
+    private static final String SELECT_ALL_CINEMAS = "select c.id as cinema_id, " +
             "c.name as cinema_name, c.logo as cinema_logo from cinema c";
 
     public static final String SELECT_CINEMA_BY_ID = "select c.id as cinema_id, " +
             "c.name as cinema_name, c.logo as cinema_logo from cinema c where id=?";
 
-    private static final String SELECT_CINEMAS_BY_MOVIE =  "select c.id as cinema_id, " +
+    private static final String SELECT_CINEMAS_BY_MOVIE = "select c.id as cinema_id, " +
             "c.name as cinema_name, c.logo as cinema_logo " +
             "from cinema c inner join movie_cinema mc on mc.cinema_id=c.id " +
             "where mc.movie_id=?";
 
 
-    private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
 
     @Override
     public List<Cinema> findAll() {
-        List<Cinema> cinemas = new ArrayList<>();
+        return jdbcTemplate.query(SELECT_ALL_CINEMAS, new CinemaMapper());
+
+        /*new ArrayList<>();
 
         try(Connection connection = dataSource.getConnection(); Statement stmt = connection.createStatement();){
             ResultSet rs = stmt.executeQuery(SELECT_ALL_CINEMAS);
@@ -43,19 +46,18 @@ public class JdbcCinemaDao implements CinemaDao {
             }
         } catch (SQLException e) {
             e.printStackTrace();
-        }
+        }*/
 
-        return cinemas;
     }
 
     @Override
     public Optional<Cinema> findById(Integer id) {
-        return Optional.empty();
+        return Optional.ofNullable(jdbcTemplate.queryForObject(SELECT_CINEMA_BY_ID, new CinemaMapper(), id));
     }
 
     @Override
     public List<Cinema> findByMovie(Movie m) {
-        return List.of();
+        return jdbcTemplate.query(SELECT_CINEMAS_BY_MOVIE, new CinemaMapper(), m.getId());
     }
 
     @Override
@@ -63,12 +65,17 @@ public class JdbcCinemaDao implements CinemaDao {
         return null;
     }
 
-    public Cinema mapRow(ResultSet rs, int rowNum) throws SQLException {
-        Cinema c = new Cinema();
-        c.setId(rs.getInt("cinema_id"));
-        c.setName(rs.getString("cinema_name"));
-        c.setLogo(rs.getString("cinema_logo"));
-        return c;
+
+    static class CinemaMapper implements RowMapper<Cinema> {
+
+        public Cinema mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Cinema c = new Cinema();
+            c.setId(rs.getInt("cinema_id"));
+            c.setName(rs.getString("cinema_name"));
+            c.setLogo(rs.getString("cinema_logo"));
+            return c;
+        }
+
     }
 
 }
