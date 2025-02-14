e9de9c09822f5bdde7571d8d42ef0b3db0c8042f
Marcin Dzierzak <m@d.pl>
2025-02-14 14:37:18 +0100

    vod - test - integration - rest assured

--- app-vod/src/test/java/vod/VodApplicationTest.java
+++ app-vod/src/test/java/vod/VodApplicationTest.java
@@ -1,27 +1,36 @@
 package vod;
 
+import io.restassured.RestAssured;
+import lombok.extern.slf4j.Slf4j;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import vod.model.Cinema;
 import vod.service.CinemaService;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("h2")
+@Slf4j
 class VodApplicationTest {
+
+    final int CINEMA_ID = 1;
+    final String CINEMA_NAME = "Cinematheque Francaise";
 
     @Autowired
     private CinemaService cinemaService;
 
+    @LocalServerPort
+    private Integer localPort;
+
     @Test
-    void contextLoads(){
+    void testCinemaServiceReturningExistingCinema() {
 
         // given
-        final int CINEMA_ID = 1;
-        final String CINEMA_NAME = "Cinematheque Francaise";
 
         // when
         Cinema cinema = cinemaService.getCinemaById(CINEMA_ID);
@@ -32,5 +41,20 @@ class VodApplicationTest {
         assertEquals(CINEMA_ID, cinema.getId());
 
     }
+
+    @Test
+    void testCinemaControllerReturningExistingCinema() {
+
+        log.info("local server port: {}", localPort);
+
+        RestAssured
+                .get("http://localhost:" + localPort + "/cinemas/" + CINEMA_ID)
+                .then()
+                .statusCode(200).assertThat()
+                .body("name", Matchers.equalTo(CINEMA_NAME))
+                .body("id", Matchers.equalTo(CINEMA_ID));
+
+    }
+
 
 }
